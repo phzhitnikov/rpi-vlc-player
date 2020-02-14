c52b3c2f3e8d6a2e11b1b84e14080b2f2fc24b03
Phil <phzhitnikov@gmail.com>
2020-02-14 05:17:15 +0300

    Cleanup

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PLAYER_BASE_PATH="/home/pi/rpi-vlc-player"
-
-# Start vlc with telnet control
-vlc --intf telnet --telnet-password admin --no-osd --fullscreen &
-
-# Start main script
-python3 $PLAYER_BASE_PATH/src/main.py &
